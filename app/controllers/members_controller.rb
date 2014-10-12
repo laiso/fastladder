122b685d137ca5a56877c585a18b2fc64ebfc8b9
laiso <laiso@lai.so>
2014-10-13 02:48:50 +0900

    Heroku Deploy
    Conflicts:
    	Gemfile.lock

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,19 +4,21 @@ class MembersController < ApplicationController
   end
 
   def create
-    cookies.delete :auth_token
-    # protects against session fixation attacks, wreaks havoc with
-    # request forgery protection.
-    # uncomment at your own risk
-    # reset_session
-    @member = Member.new(member_params)
-    @member.save!
-    session[:member_id] = @member.id
-    redirect_to '/'
-    flash[:notice] = "Thanks for signing up!"
-  rescue ActiveRecord::RecordInvalid
-    flash[:error] = @member.errors.map{|x, y| "#{x}: #{y}"}.join(', ')
-    render action: 'new'
+  #  Sign Up is freezed.
+  #
+  #   cookies.delete :auth_token
+  #   # protects against session fixation attacks, wreaks havoc with
+  #   # request forgery protection.
+  #   # uncomment at your own risk
+  #   # reset_session
+  #   @member = Member.new(member_params)
+  #   @member.save!
+  #   session[:member_id] = @member.id
+  #   redirect_to '/'
+  #   flash[:notice] = "Thanks for signing up!"
+  # rescue ActiveRecord::RecordInvalid
+  #   flash[:error] = @member.errors.map{|x, y| "#{x}: #{y}"}.join(', ')
+  #   render action: 'new'
   end
 
   private
